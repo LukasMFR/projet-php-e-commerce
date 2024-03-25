437475dc33bcc14f2a5b612d45357ff05eab5a9e
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-03-25 18:09:07 +0100

    Change index.php & style.css with AutoCar

--- index.php
+++ index.php
@@ -1,20 +1,31 @@
-<!-- index.php -->
+<?php
+// Simuler une petite base de données de produits en utilisant des tableaux associatifs
+$produits = [
+    ['id' => 1, 'nom' => 'Voiture Modèle X', 'prix' => '50000', 'categorie' => 'voiture'],
+    ['id' => 2, 'nom' => 'Voiture Modèle Y', 'prix' => '60000', 'categorie' => 'voiture'],
+    ['id' => 3, 'nom' => 'Puff Modèle A', 'prix' => '75', 'categorie' => 'puff'],
+    ['id' => 4, 'nom' => 'Puff Modèle B', 'prix' => '85', 'categorie' => 'puff'],
+];
+?>
+
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>AutoCar</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <meta charset="UTF-8">
+    <title>AutoCar - Votre magasin de voitures et puffs</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <h1>AutoCar</h1>
-    <p>Site web de AutoCar</p>
-
-    <?php
-    echo "Hello, World!";
-    echo "<br>";
-    echo "The current server time is: " . date('Y-m-d H:i:s');
-    echo "<br>";
-    echo $_SERVER['HTTP_USER_AGENT'];
-    ?>
+    <h1>Bienvenue chez AutoCar</h1>
+    <h2>Nos produits</h2>
+    <div class="produits">
+        <?php foreach ($produits as $produit): ?>
+            <div class="produit">
+                <h3><?php echo $produit['nom']; ?></h3>
+                <p>Prix: <?php echo $produit['prix']; ?> €</p>
+                <p>Catégorie: <?php echo $produit['categorie']; ?></p>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </body>
 </html>
