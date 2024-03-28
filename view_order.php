c7ec76da4666a57222717040ca6614a39f8d94da
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-03-28 09:11:57 +0100

    Update lang="en" to lang="fr"

--- view_order.php
+++ view_order.php
@@ -28,7 +28,7 @@
 	<?php include 'style.css'; ?>
 </style>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
