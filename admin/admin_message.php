e2fc0389fb194df6953354cacfb5c47036a00628
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-03-28 20:08:51 +0100

    Update admin_message.php

--- admin/admin_message.php
+++ admin/admin_message.php
@@ -19,9 +19,9 @@
 		if ($verify_delete->rowCount() > 0) {
 			$delete_review = $conn->prepare("DELETE FROM `message` WHERE id = ?");
 			$delete_review->execute([$delete_id]);
-			$success_msg[] = "message Deleted";
+			$success_msg[] = "Message supprimé";
 		}else{
-			$warning_msg[] = 'message Already Deleted';
+			$warning_msg[] = 'Message déjà supprimé';
 		}
 	}
 ?>
@@ -35,20 +35,20 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- font awesome cdn link  -->
    	<link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
-	<title>admin dashboard</title>
+	<title>Messages - AutoCar</title>
 </head>
 <body>
 	
 		<?php include '../components/admin_header.php'; ?>
 		<div class="main">
 			<div class="banner">
-				<h1>user's messages</h1>
+				<h1>Messages des utilisateurs</h1>
 			</div>
 			<div class="title2">
-				<a href="home.php">home </a><span>/ messages</span>
+				<a href="home.php">Accueil </a><span>/ Messages</span>
 			</div>
 		<section class="message-container">
-		<div class="heading"><h1>user's messages</h1></div>
+		<div class="heading"><h1>Messages des utilisateurs</h1></div>
 		<div class="box-container">
 			<?php 
 				$select_reviews = $conn->prepare("SELECT * FROM `message`");
@@ -66,14 +66,14 @@
 					<form action="" method="post" class="flex-btn">
 						<input type="hidden" name="delete_id" value="<?= $fetch_review['id']; ?>">
 						
-						<input type="submit" name="delete_review" value="delete message" class="btn" onclick="return confirm('delete this review');">
+						<input type="submit" name="delete_review" value="Supprimer le message" class="btn" onclick="return confirm('Supprimer ce message ?');">
 					</form>
 				
 			</div>
 			<?php 
 					}
 				}else{
-					echo '<p class="empty">no messages added yet!</p>';
+					echo '<p class="empty">Aucun message ajouté pour le moment !</p>';
 				}
 			?>
 		</div>
