2c924cb20febb1c4d681276438b4847ce8f92f47
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-03-28 19:32:44 +0100

    Update admin_header.php

--- components/admin_header.php
+++ components/admin_header.php
@@ -2,10 +2,10 @@
 	<div class="flex">
 		<a href="home.php" class="logo"><img src="../img/logo.jpg"></a>
 		<nav class="navbar">
-			<a href="dashboard.php">dashboard</a>
-			<a href="add_posts.php">add product</a>
-			<a href="view_posts.php">view post</a>
-			<a href="user_accounts.php">accounts</a>
+			<a href="dashboard.php">Tableau de bord</a>
+			<a href="add_posts.php">Ajouter un produit</a>
+			<a href="view_posts.php">Voir les produits</a>
+			<a href="user_accounts.php">Comptes</a>
 		</nav>
 		<div class="icons">
 			<i class="bx bxs-user" id="user-btn"></i>
@@ -25,14 +25,10 @@
 				<p><?= $fetch_profile['name']; ?></p>
 			</div>
 			<div class="flex-btn">
-				<a href="update_profile.php" class="btn">update profile</a>
-				<a href="../components/admin_logout.php" onclick="return confirm('logout from this website')" class="btn">logout</a>
+				<a href="update_profile.php" class="btn">Mettre à jour le profil</a>
+				<a href="../components/admin_logout.php" onclick="return confirm('Se déconnecter du site ?')" class="btn">Se déconnecter</a>
 			</div>
 			<?php }?>
 		</div>
 	</div>
 </header>
-
-
-
-
