3ce92d883fd667840ec9a4fe64cfd3030d8ff84a
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-03-31 12:29:00 +0200

    Format & add navbar style to admin navbar

--- components/admin_header.php
+++ components/admin_header.php
@@ -1,6 +1,9 @@
 <header class="header">
 	<div class="flex">
-		<a href="dashboard.php" class="logo"><img src="../img/logo.jpg"></a>
+		<a href="home.php" class="brand">
+			<div class="logo"><img src="../img/favicon-128.png" alt="AutoCar Logo"></div>
+			<h1>AutoCar</h1>
+		</a>
 		<nav class="navbar">
 			<a href="dashboard.php">Tableau de bord</a>
 			<a href="add_posts.php">Ajouter un produit</a>
@@ -9,27 +12,30 @@
 		</nav>
 		<div class="icons">
 			<i class="bx bxs-user" id="user-btn"></i>
-			
+
 			<i class='bx bx-list-plus' id="menu-btn" style="font-size: 2rem;"></i>
 		</div>
 		<div class="profile-detail">
-			<?php 
-				$select_profile = $conn->prepare("SELECT * FROM `admin` WHERE id=?");
-				$select_profile->execute([$admin_id]);
-				if ($select_profile->rowCount() > 0) {
-					$fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
-				
-			?>
-			<div class="profile">
-				<img src="../image/<?= $fetch_profile['profile']; ?>" class="logo-image" width="100">
-				<p><?= $fetch_profile['name']; ?></p>
-			</div>
-			<div class="flex-btn">
-				<a href="update_profile.php" class="btn">Mettre à jour le profil</a>
-				<a href="../components/admin_logout.php" onclick="return confirm('Se déconnecter du site ?')" class="btn">Se déconnecter</a>
-			</div>
-			<a href="../index.php" class="btn">Retourner au site visiteur</a>
-			<?php }?>
+			<?php
+			$select_profile = $conn->prepare("SELECT * FROM `admin` WHERE id=?");
+			$select_profile->execute([$admin_id]);
+			if ($select_profile->rowCount() > 0) {
+				$fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+
+				?>
+				<div class="profile">
+					<img src="../image/<?= $fetch_profile['profile']; ?>" class="logo-image" width="100">
+					<p>
+						<?= $fetch_profile['name']; ?>
+					</p>
+				</div>
+				<div class="flex-btn">
+					<a href="update_profile.php" class="btn">Mettre à jour le profil</a>
+					<a href="../components/admin_logout.php" onclick="return confirm('Se déconnecter du site ?')"
+						class="btn">Se déconnecter</a>
+				</div>
+				<a href="../index.php" class="btn">Retourner au site visiteur</a>
+			<?php } ?>
 		</div>
 	</div>
 </header>
