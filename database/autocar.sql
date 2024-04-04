9f1fa250913baf4e81493455a9b721b4fe573a62
Hillel3009 <161851232+Hillel3009@users.noreply.github.com>
2024-04-04 18:53:08 +0200

    modif du sql

--- database/autocar.sql
+++ database/autocar.sql
@@ -3,217 +3,194 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 02, 2023 at 04:22 PM
 -- Server version: 10.4.25-MariaDB
--- PHP Version: 7.4.30
+
 
 DROP DATABASE IF EXISTS autocar;
 CREATE DATABASE autocar ;
 USE autocar ;
 DROP TABLE IF EXISTS admin ;
-DROP TABLE IF EXISTS cart ;
+DROP TABLE IF EXISTS panier ;
 DROP TABLE IF EXISTS message ;
-DROP TABLE IF EXISTS orders ;
-DROP TABLE IF EXISTS products ;
-DROP TABLE IF EXISTS users ;
-DROP TABLE IF EXISTS wishlist ;
+DROP TABLE IF EXISTS commande ;
+DROP TABLE IF EXISTS vehicule ;
+DROP TABLE IF EXISTS Puff ;
+DROP TABLE IF EXISTS utilisateur ;
+DROP TABLE IF EXISTS Favoris ;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `autocar`
---
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
-  `id` int(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `profile` varchar(255) NOT NULL
+  `idAdmin` int(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `mot de passe` varchar(50) NOT NULL,
+  `profile` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `admin`
---
 
-INSERT INTO `admin` (`id`, `name`, `email`, `password`, `profile`) VALUES
+-- Dumping data for table `admin`
+
+INSERT INTO `admin` (`idAdmin`, `nom`, `mail`, `mot de passe`, `profile`) VALUES
 (1, 'admin', 'admin@autocar.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', 'glitch.jpg');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `cart`
---
 
-CREATE TABLE `cart` (
-  `id` varchar(20) NOT NULL,
-  `user_id` varchar(20) NOT NULL,
-  `product_id` varchar(20) NOT NULL,
-  `price` varchar(10) NOT NULL,
-  `qty` varchar(2) NOT NULL DEFAULT '1'
+-- Table structure for table `panier`
+
+CREATE TABLE `panier` (
+  `idpanier` varchar(20) NOT NULL,
+  `idUser` varchar(20) NOT NULL,
+  `IDVech` varchar(20) NOT NULL,
+  `IDPuff` varchar(20) NOT NULL,
+  `Prix` decimal, NOT NULL,
+  `Quantité` int, NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `cart`
---
 
-INSERT INTO `cart` (`id`, `user_id`, `product_id`, `price`, `qty`) VALUES
-('UzH7ynJfxmARltq6q5Sx', 'UAVjN46f0bvXSKquej8S', 'aSBHDzG26iXurm6cfoNv', '50', '1');
+-- Dumping data for table `panier`
 
--- --------------------------------------------------------
+INSERT INTO `cart` (`idpanier`, `idUser`, `IDVech`, `IDPuff`, `Prix`, 'Quantité') VALUES
+('UzH7ynJfxmARltq6q5Sx', 'UAVjN46f0bvXSKquej8S', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv' '50', '1');
 
---
+
+
 -- Table structure for table `message`
---
 
-CREATE TABLE `message` (
-  `id` varchar(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `subject` varchar(255) NOT NULL,
-  `message` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--CREATE TABLE `message` (
+ -- `id` varchar(255) NOT NULL,
+  --`user_id` varchar(255) NOT NULL,
+ -- `name` varchar(255) NOT NULL,
+  --`email` varchar(100) NOT NULL,
+ -- `subject` varchar(255) NOT NULL,
+ -- `message` varchar(255) NOT NULL
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
+
 -- Dumping data for table `message`
---
 
 INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `subject`, `message`) VALUES
-('0', '0', 'Paul', 'paul.roux@gmail.com', 'shop', 'good'),
-('Lm7uFQVcX3czwG0yX5p0', 'UAVjN46f0bvXSKquej8S', 'Paul', 'paul.roux@gmail.com', 'maths,science', 'kk');
+('0', '0', 'Paul', 'paul.depont@gmail.com', 'shop', 'good'),
+('Lm7uFQVcX3czwG0yX5p0', 'UAVjN46f0bvXSKquej8S', 'Paul', 'paul.depont@gmail.com', 'maths,science', 'kk');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `orders`
---
 
-CREATE TABLE `orders` (
-  `id` varchar(20) NOT NULL,
-  `user_id` varchar(20) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `address` varchar(200) NOT NULL,
-  `address_type` varchar(10) NOT NULL,
-  `method` varchar(50) NOT NULL,
-  `product_id` varchar(20) NOT NULL,
-  `price` varchar(10) NOT NULL,
-  `qty` varchar(2) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `status` varchar(50) NOT NULL DEFAULT 'en cours',
-  `payment_status` varchar(100) NOT NULL
+-- Table structure for table `commande`
+
+CREATE TABLE `commande` (
+  `idCommande` varchar(50) NOT NULL,
+  `IDUser` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `Num` varchar(50) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `Adresse` varchar(100) NOT NULL
+  `MethodeAchat` varchar(50) NOT NULL,
+  `IDVech` varchar(50) NOT NULL,
+  `IDPuff` varchar(50) NOT NULL,
+  `Prix` decimal, NOT NULL,
+  `Quantité` int, NOT NULL,
+  `DateAchat` date, NOT NULL DEFAULT,current_timestamp(),
+  `statuPayement` varchar(50) NOT NULL DEFAULT 'en cours',
+  
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `orders`
---
 
-INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `address`, `address_type`, `method`, `product_id`, `price`, `qty`, `date`, `status`, `payment_status`) VALUES
-('EYZ94PhWrzea0s9Tdd2J', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'BLTtlhOgq1cuz7plh4Ia', '123', '1', '2023-02-28', 'annulee', 'en attente'),
-('DStPLCBmD0m0OjAFYlhg', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'jo35YMmBWpvbCMB65UdA', '160', '1', '2023-02-28', 'annulee', 'en attente'),
-('XyoWmad14f2YOWbi11XF', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'aSBHDzG26iXurm6cfoNv', '50', '1', '2023-02-28', 'annulee', 'en attente'),
-('OGTzld6EmHmNHeXZQkB6', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'uOarNNg0n3KD9OvPtItP', '80', '1', '2023-02-28', 'annulee', 'en attente'),
-('UUFMa328sIAdb3znDXce', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '10 avenue Montaigne, 75008 Paris', 'home', 'credit or debit card', 'kun96OpQed6Eww6M1URo', '120', '1', '2023-02-28', 'annulee', 'completee'),
-('Bsatz7miuWWgXMEx5qzW', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'kun96OpQed6Eww6M1URo', '120', '1', '2023-02-28', 'en cours', 'completee'),
-('4SJfc2GJY4ekJN45CKbP', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'BLTtlhOgq1cuz7plh4Ia', '123', '1', '2023-02-28', 'en cours', 'en attente'),
-('Jd0yGYljvlchrTLd5KGQ', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '18 rue Oberkampf, 75011 Paris', 'office', 'credit or debit card', 'BLTtlhOgq1cuz7plh4Ia', '123', '1', '2023-02-28', 'en cours', 'completee'),
-('wtyNDfBfSwShC9FXFnbC', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '18 rue Oberkampf, 75011 Paris', 'office', 'credit or debit card', 'aSBHDzG26iXurm6cfoNv', '50', '1', '2023-02-28', 'en cours', 'en attente'),
-('KRbSyH7ZgbVzWyyZQoiv', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '18 rue Oberkampf, 75011 Paris', 'office', 'credit or debit card', 'uOarNNg0n3KD9OvPtItP', '80', '1', '2023-02-28', 'en cours', 'en attente'),
-('9vucKr2sSPqcIUidPedP', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'office', 'cash on delivery', 'kun96OpQed6Eww6M1URo', '120', '1', '2023-02-28', 'en cours', 'en attente'),
-('gq2RDUuhaPe7TDcxiGCy', 'UAVjN46f0bvXSKquej8S', 'Paul', '0675001234', 'paul.roux@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'home', 'cash on delivery', 'g5DLcNHmtHvq3DtJYsCb', '80', '1', '2023-02-28', 'en cours', 'en attente'),
-('JqyfHoT9UzR4qcvp3LNJ', 'd5URvsP8VusCXQoCdMBG', 'Camille', '0675001234', 'camille.blanchard@gmail.com', '507A, 24 back side, mumbai, India, 110019', 'home', 'credit or debit card', 'BLTtlhOgq1cuz7plh4Ia', '123', '1', '2023-02-28', 'en cours', 'completee'),
-('yyD4B276Pg9lfGpRjcr9', 'd5URvsP8VusCXQoCdMBG', 'Camille', '0675045678', 'camille.blanchard@gmail.com', '507A, 24 back side, mumbai, india, 112233', 'office', 'credit or debit card', 'jo35YMmBWpvbCMB65UdA', '160', '2', '2023-02-28', 'annulee', 'en attente');
+-- Dumping data for table `commande`
 
--- --------------------------------------------------------
+INSERT INTO `orders` (`idcommande`, `IDUser`, `nom`, `Prenom`, `Num`, `mail`, `Adresse`, `MethodAchat`, `IDVech`, `IDPuff`, `Prix`, `Quantite`, `DateAchat`, `StatuPayement`) VALUES
+('EYZ94PhWrzea0s9Tdd2J', 'UAVjN46f0bvXSKquej8S', 'Paul', 'dupont', '0675001234', 'paul.depont@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'cash on delivery', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv', '123', '1', '2023-02-28',  'en attente'),
+('DStPLCBmD0m0OjAFYlhg', 'UAVjN46f0bvXSKquej8S', 'Paul', 'dupont', '0675001234', 'paul.depont@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'cash on delivery', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv', '160', '1', '2023-02-28', 'en attente'),
+('UUFMa328sIAdb3znDXce', 'UAVjN46f0bvXSKquej8S', 'Paul', 'dupont', '0675001234', 'paul.depont@gmail.com', '10 avenue Montaigne,  75008 Paris', 'credit or debit card', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv', '120', '1', '2023-02-28', 'completee'),
+('9vucKr2sSPqcIUidPedP', 'UAVjN46f0bvXSKquej8S', 'Paul', 'dupont', '0675001234', 'paul.depont@gmail.com', '5 avenue Victor Hugo, 75016 Paris','cash on delivery', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv','120', '1', '2023-02-28', 'en attente'),
+('gq2RDUuhaPe7TDcxiGCy', 'UAVjN46f0bvXSKquej8S', 'Paul', 'dupont', '0675001234', 'paul.depont@gmail.com', '5 avenue Victor Hugo, 75016 Paris', 'cash on delivery', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv', '80', '1', '2023-02-28', 'en attente'),
+('JqyfHoT9UzR4qcvp3LNJ', 'd5URvsP8VusCXQoCdMBG', 'Camille', 'dupont', '0675001234', 'camille.lelouche@gmail.com', '507A, 24 back side, mumbai, India, 110019', 'credit or debit card', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv', '123', '1', '2023-02-28', 'completee'),
+('yyD4B276Pg9lfGpRjcr9', 'd5URvsP8VusCXQoCdMBG', 'Camille', 'dupont', '0675045678', 'camille.lelouche@gmail.com', '507A, 24 back side, mumbai, india, 112233', 'credit or debit card', 'aSBHDzG26iXurm6cfoNv', 'aSBHDzG26iXurm6cfoNv', '160', '2', '2023-02-28', 'en attente');
 
---
--- Table structure for table `products`
---
 
-CREATE TABLE `products` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `product_detail` varchar(500) NOT NULL,
-  `status` varchar(100) NOT NULL
+
+-- Table structure for table `vehicule`
+
+CREATE TABLE `vehicule` (
+  `idvech` varchar(50) NOT NULL,
+  `marque` varchar(255) NOT NULL,
+  `modele` varchar(255) NOT NULL,
+  `anneeFab` varchar(255) NOT NULL,
+  `prix` decimal, NOT NULL,
+  'image' varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `products`
---
 
-INSERT INTO `products` (`id`, `name`, `price`, `image`, `product_detail`, `status`) VALUES
-('BLTtlhOgq1cuz7plh4Ia', 'Lemon Green Tea', '123', '8lXs7FvkBvchy7RijAzE.jpg', 'Lorem ipsum', 'inactif'),
-('jo35YMmBWpvbCMB65UdA', 'Kabusecha Green Tea', '160', 'vKK7WLWKiqhMBVltCovA.jpg', 'Lorem ipsum', 'actif'),
-('aSBHDzG26iXurm6cfoNv', 'Gyokuro Green Tea', '50', 'OyP5om4L0iRn9ES2sTPz.jpg', 'Lorem ipsum', 'actif'),
-('g5DLcNHmtHvq3DtJYsCb', 'Sweet Lemon Iced Tea', '80', 'FKPiM5OkUoYVwHVIP2uz.jpg', 'Lorem ipsum', 'actif'),
-('uOarNNg0n3KD9OvPtItP', 'Lemon Verbena Tea', '80', 'EJsJ87nGcHHHod779fKs.jpg', 'Lorem ipsum', 'actif'),
-('26lPPTjXh9EkNc7WocS5', 'Longjing Tea', '70', 'dkqSpkWuMj94JicCEqHs.jpg', 'Lorem ipsum', 'actif'),
-('kun96OpQed6Eww6M1URo', 'Gunpowder Tea', '120', '8lXs7FvkBvchy7RijAzE.jpg', 'Lorem ipsum', 'actif'),
-('wrJTrzoHsuEwr7hGi3R6', 'Minty Lemon Iced Tea', '95', 'awmvYZRs2wNZh0yFjoSk.jpg', 'Lorem ipsum', 'actif'),
-('eBbtkVNYiJJKT9mCgYbk', 'tea', '200', 'card2.jpg', 'Lorem ipsum', 'actif'),
-('bEvIK2PvwOqY4l8nuPTZ', 'green herbal autocar', '23', 'YsMZ5Bezou9eH1KhTZrZ.jpg', 'Lorem ipsum', 'actif');
+-- Dumping data for table `vehicule`
 
--- --------------------------------------------------------
+INSERT INTO `products` (`idvech`, `marque`, `modele`, `anneFab`, `prix`, `image`) VALUES
+('BLTtlhOgq1cuz7plh4Ia', 'Ferari', 'SP51', '2022', '10000', 'ferariesp51.jpg'),
+('jo35YMmBWpvbCMB65UdA', 'Bugatti', 'voiture noir', '2021', '10000', 'bugatienoir.webp'),
+('aSBHDzG26iXurm6cfoNv', 'Aston Martin', 'Vantage GT3', '2018/2019', '10000', 'astonmartine.jpg');
 
---
--- Table structure for table `users`
---
 
-CREATE TABLE `users` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `user_type` varchar(255) NOT NULL DEFAULT 'user'
+
+
+-- Table structure for table `puff`
+
+CREATE TABLE `Puff` (
+  `idPuff` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `saveur` Text(50) NOT NULL,
+  `prix` decimal, NOT NULL,
+  `stock` int, NOT NULL,
+  'image' varchar(255) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
-('UAVjN46f0bvXSKquej8S', 'Paul', 'paul.roux@gmail.com', 'paul786', 'user'),
-('ir7qjxTxaQm9PM5drpEn', 'Sophie', 'sophie.moreau@gmail.com', 'sophie786', 'user'),
-('GE2LLAWjKATiQRLHaa6O', 'Maxime', 'maxime.petit@gmail.com', '12345', 'user'),
-('d5URvsP8VusCXQoCdMBG', 'Camille', 'camille.blanchard@gmail.com', '12345', 'user');
+-- Dumping data for table `puff`
 
--- --------------------------------------------------------
+INSERT INTO `products` (`idPuff`, `nom`, `saveur`, `prix`, `stock`, `image`) VALUES
+('BLTtlhOgq1cuz7plh4Ia', 'Car Vape', '12', 'fraise/gasoile', '12', '2000', 'puffFraiseGasoile.jpg');
 
---
--- Table structure for table `wishlist`
---
 
-CREATE TABLE `wishlist` (
-  `id` varchar(255) NOT NULL,
-  `user_id` varchar(255) NOT NULL,
-  `product_id` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL
+
+-- Table structure for table `utilisateur`
+
+CREATE TABLE `utilisateur` (
+  `idUser` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `mail` varchar(255) NOT NULL,
+  `motDePasse` varchar(255) NOT NULL DEFAULT 
+  `typeUser` varchar(255) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `wishlist`
---
 
-INSERT INTO `wishlist` (`id`, `user_id`, `product_id`, `price`) VALUES
-('FngaVJmk3vBeq3KUmt03', 'UAVjN46f0bvXSKquej8S', 'jo35YMmBWpvbCMB65UdA', '160');
+-- Dumping data for table `utilisateur`
+
+INSERT INTO `users` (`idUser`, `nom`, `prenom`, `mail`, `motDePasse`, 'TypeUser' ) VALUES
+('UAVjN46f0bvXSKquej8S', 'Paul', 'dupont', 'paul.depont@gmail.com', 'paul786', 'user'),
+('d5URvsP8VusCXQoCdMBG', 'Camille', 'lelouche', 'camille.lelouche@gmail.com', '12345', 'user');
+
+
+
+-- Table structure for table `favoris`
+
+CREATE TABLE `favoris` (
+  `idFavoris` varchar(255) NOT NULL,
+  `idUser` varchar(255) NOT NULL,
+  `IDVech` varchar(255) NOT NULL,
+  `IDPuff` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- Dumping data for table `favoris`
+
+INSERT INTO `Favoris` (`idFavoris`, `IDUser`, `IDVech`, `IDPuff`) VALUES
+();
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
