d7b8fd1791ac9efbda2b271fd31ff7fc70597ada
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-04-05 19:49:18 +0200

    Format contact.php 2

--- contact.php
+++ contact.php
@@ -1,28 +1,30 @@
-<?php 
- include 'components/connection.php';
- session_start();
- if (isset($_SESSION['user_id'])) {
-		$user_id = $_SESSION['user_id'];
-	}else{
-		$user_id = '';
-	}
+<?php
+include 'components/connection.php';
+session_start();
+if (isset($_SESSION['user_id'])) {
+	$user_id = $_SESSION['user_id'];
+} else {
+	$user_id = '';
+}
 
-	if (isset($_POST['logout'])) {
-		session_destroy();
-		header("location: login.php");
-	}
+if (isset($_POST['logout'])) {
+	session_destroy();
+	header("location: login.php");
+}
 ?>
 <style type="text/css">
 	<?php include 'style.css'; ?>
 </style>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 	<title>Contact - Road Luxury</title>
 </head>
+
 <body>
 	<?php include 'components/header.php'; ?>
 	<div class="main">
@@ -65,65 +67,67 @@
 			</div>
 		</section> -->
 		<div class="form-container">
-			 <form method="post">
-			 	<div class="title">
-			 		<img src="img/download.png" class="logo">
-			 		<h1>Laissez un message</h1>
-			 	</div>
-			 	<div class="input-field">
-			 		<p>Votre nom <sup>*</sup></p>
-			 		<input type="text" name="name">
-			 	</div>
-			 	<div class="input-field">
-			 		<p>Votre email <sup>*</sup></p>
-			 		<input type="email" name="email">
-			 	</div>
-			 	<div class="input-field">
-			 		<p>Votre numéro <sup>*</sup></p>
-			 		<input type="text" name="number">
-			 	</div>
-			 	<div class="input-field">
-			 		<p>Votre message <sup>*</sup></p>
-			 		<textarea name="message"></textarea>
-			 	</div>
-			 	<button type="submit" name="submit-btn" class="btn">Envoyer le message</button>
-			 </form>
+			<form method="post">
+				<div class="title">
+					<img src="img/download.png" class="logo">
+					<h1>Laissez un message</h1>
+				</div>
+				<div class="input-field">
+					<p>Votre nom <sup>*</sup></p>
+					<input type="text" name="name">
+				</div>
+				<div class="input-field">
+					<p>Votre email <sup>*</sup></p>
+					<input type="email" name="email">
+				</div>
+				<div class="input-field">
+					<p>Votre numéro <sup>*</sup></p>
+					<input type="text" name="number">
+				</div>
+				<div class="input-field">
+					<p>Votre message <sup>*</sup></p>
+					<textarea name="message"></textarea>
+				</div>
+				<button type="submit" name="submit-btn" class="btn">Envoyer le message</button>
+			</form>
 		</div>
-		
+
 		<div class="address">
-			 	<div class="title">
-			 		<img src="img/download.png" class="logo">
-			 		<h1>Détails de contact</h1>
-			 		<p>Nous sommes là pour répondre à toutes vos questions. N'hésitez pas à nous contacter par mail, téléphone ou via le formulaire.</p>
-			 	</div>
-			 	<div class="box-container">
-			 		<div class="box">
-			 			<i class="bx bxs-map-pin"></i>
-			 			<div>
-			 				<h4>Adresse</h4>
-			 				<p>10 Rue de Rivoli, 75001 Paris</p>
-			 			</div>
-			 		</div>
-			 		<div class="box">
-			 			<i class="bx bxs-phone-call"></i>
-			 			<div>
-			 				<h4>Numéro de téléphone</h4>
-			 				<p>+33 6 12 34 56 78</p>
-			 			</div>
-			 		</div>
-			 		<div class="box">
-			 			<i class="bx bxs-map-pin"></i>
-			 			<div>
-			 				<h4>Email</h4>
-			 				<p>contact@roadluxury.com</p>
-			 			</div>
-			 		</div>
-			 	</div>
-			 </div>
+			<div class="title">
+				<img src="img/download.png" class="logo">
+				<h1>Détails de contact</h1>
+				<p>Nous sommes là pour répondre à toutes vos questions. N'hésitez pas à nous contacter par mail,
+					téléphone ou via le formulaire.</p>
+			</div>
+			<div class="box-container">
+				<div class="box">
+					<i class="bx bxs-map-pin"></i>
+					<div>
+						<h4>Adresse</h4>
+						<p>10 Rue de Rivoli, 75001 Paris</p>
+					</div>
+				</div>
+				<div class="box">
+					<i class="bx bxs-phone-call"></i>
+					<div>
+						<h4>Numéro de téléphone</h4>
+						<p>+33 6 12 34 56 78</p>
+					</div>
+				</div>
+				<div class="box">
+					<i class="bx bxs-map-pin"></i>
+					<div>
+						<h4>Email</h4>
+						<p>contact@roadluxury.com</p>
+					</div>
+				</div>
+			</div>
+		</div>
 		<?php include 'components/footer.php'; ?>
 	</div>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 	<script src="script.js"></script>
 	<?php include 'components/alert.php'; ?>
 </body>
+
 </html>
