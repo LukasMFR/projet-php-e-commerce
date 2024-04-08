7bd991e19966f74b92b900b436b86167866305b8
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-04-08 13:03:20 +0200

    Squashed commit of the following:
    commit 7f75cbe0839c7a76bbab2dfb523f7bc85d5ee4b3
    Author: Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
    Date:   Mon Apr 8 12:28:46 2024 +0200
    
        Format about.php
    
    commit 0fba9dedac0e70d64a43a55919aac27b2918252b
    Author: Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
    Date:   Mon Apr 8 12:12:11 2024 +0200
    
        Update README.md
    
    commit ae84a52fc410b360393276b28784d93b338921a5
    Merge: 42a2776 194223d
    Author: Anis Fetouab <91206298+AnisFetouab@users.noreply.github.com>
    Date:   Mon Apr 8 12:10:54 2024 +0200
    
        Merge pull request #21 from LukasMFR/main-anis
    
        Main anis

--- about.php
+++ about.php
@@ -1,28 +1,30 @@
-<?php 
- include 'components/connection.php';
- session_start();
- if (isset($_SESSION['user_id'])) {
-		$user_id = $_SESSION['user_id'];
-	}else{
-		$user_id = '';
-	}
+<?php
+include 'components/connection.php';
+session_start();
+if (isset($_SESSION['user_id'])) {
+	$user_id = $_SESSION['user_id'];
+} else {
+	$user_id = '';
+}
 
-	if (isset($_POST['logout'])) {
-		session_destroy();
-		header("location: login.php");
-	}
+if (isset($_POST['logout'])) {
+	session_destroy();
+	header("location: login.php");
+}
 ?>
 <style type="text/css">
 	<?php include 'style.css'; ?>
 </style>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 	<title>À propos - Road Luxury</title>
 </head>
+
 <body>
 	<?php include 'components/header.php'; ?>
 	<div class="main">
@@ -32,7 +34,7 @@
 		<div class="title2">
 			<a href="home.php">accueil </a><span>/ à propos</span>
 		</div>
-		
+
 		<section class="services">
 			<div class="title">
 				<img src="img/download.png" class="logo">
@@ -77,16 +79,17 @@
 				</div>
 				<div class="detail">
 					<h1>Visitez notre magnifique showroom !</h1>
-					<p>Notre showroom est la vitrine de notre passion pour l'automobile ; 
-						une créativité déployée dans la présentation de nos voitures. 
-						Que vous recherchiez le véhicule parfait pour un événement spécial, 
-						ou que vous souhaitiez simplement élever votre expérience de conduite avec un véhicule d'exception, 
+					<p>Notre showroom est la vitrine de notre passion pour l'automobile ;
+						une créativité déployée dans la présentation de nos voitures.
+						Que vous recherchiez le véhicule parfait pour un événement spécial,
+						ou que vous souhaitiez simplement élever votre expérience de conduite avec un véhicule
+						d'exception,
 						nous sommes là pour vous accompagner.</p>
-                    <a href="view_products.php" class="btn">Acheter maintenant</a>
+					<a href="view_products.php" class="btn">Acheter maintenant</a>
 				</div>
 			</div>
 		</div>
-<section class="thumb">
+		<section class="thumb">
 			<div class="box-container">
 				<div class="box">
 					<img src="img/thumb2.jpg">
@@ -124,31 +127,35 @@
 				<img src="img/download.png" class="logo">
 				<h1>Ce que nos clients disent de nous</h1>
 				<p>Leur satisfaction, notre réussite.</p>
-            </div>
-                <div class="container">
-                	<div class="testimonial-item active">
-                		<img src="img/01.jpg">
-                		<h1>Emma Leroux</h1>
-                		<p>Achat d'une Peugeot 308 - une expérience sans faute du début à la fin. Conseils experts et service clientèle au top.</p>
-                	</div>
-                	<div class="testimonial-item">
-                		<img src="img/02.jpg">
-                		<h1>Thomas Girard</h1>
-                		<p>J'ai acheté une Renault Clio récemment, et je suis plus que satisfaite. La voiture est parfaite et le processus d'achat a été fluide et agréable.</p>
-                	</div>
-                	<div class="testimonial-item">
-                		<img src="img/03.jpg">
-                		<h1>Chloé Petit</h1>
-                		<p>J'ai opté pour un pack voiture + puffs pour ma nouvelle Citroën C3. Non seulement la voiture est incroyable, mais les puffs ajoutent une touche unique à mon intérieur.</p>
-                	</div>
-                	<div class="testimonial-item">
-                		<img src="img/04.png">
-                		<h1>Marie Dubois</h1>
-                		<p>Incroyable expérience chez vous ! La Tesla Model 3 est un rêve devenu réalité, et les puffs personnalisés la rendent encore plus spéciale. Merci pour tout !</p>
-                	</div>
-                	<div class="left-arrow" onclick="nextSlide()"><i class="bx bxs-left-arrow-alt"></i></div>
-                	<div class="right-arrow" onclick="prevSlide()"><i class="bx bxs-right-arrow-alt"></i></div>
-                </div>
+			</div>
+			<div class="container">
+				<div class="testimonial-item active">
+					<img src="img/01.jpg">
+					<h1>Emma Leroux</h1>
+					<p>Achat d'une Peugeot 308 - une expérience sans faute du début à la fin. Conseils experts et
+						service clientèle au top.</p>
+				</div>
+				<div class="testimonial-item">
+					<img src="img/02.jpg">
+					<h1>Thomas Girard</h1>
+					<p>J'ai acheté une Renault Clio récemment, et je suis plus que satisfaite. La voiture est parfaite
+						et le processus d'achat a été fluide et agréable.</p>
+				</div>
+				<div class="testimonial-item">
+					<img src="img/03.jpg">
+					<h1>Chloé Petit</h1>
+					<p>J'ai opté pour un pack voiture + puffs pour ma nouvelle Citroën C3. Non seulement la voiture est
+						incroyable, mais les puffs ajoutent une touche unique à mon intérieur.</p>
+				</div>
+				<div class="testimonial-item">
+					<img src="img/04.png">
+					<h1>Marie Dubois</h1>
+					<p>Incroyable expérience chez vous ! La Tesla Model 3 est un rêve devenu réalité, et les puffs
+						personnalisés la rendent encore plus spéciale. Merci pour tout !</p>
+				</div>
+				<div class="left-arrow" onclick="nextSlide()"><i class="bx bxs-left-arrow-alt"></i></div>
+				<div class="right-arrow" onclick="prevSlide()"><i class="bx bxs-right-arrow-alt"></i></div>
+			</div>
 		</div>
 		<?php include 'components/footer.php'; ?>
 	</div>
@@ -158,17 +165,18 @@
 		let slides = document.querySelectorAll('.testimonial-item');
 		let index = 0;
 
-		function nextSlide(){
-		    slides[index].classList.remove('active');
-		    index = (index + 1) % slides.length;
-		    slides[index].classList.add('active');
+		function nextSlide() {
+			slides[index].classList.remove('active');
+			index = (index + 1) % slides.length;
+			slides[index].classList.add('active');
 		}
-		function prevSlide(){
-		    slides[index].classList.remove('active');
-		    index = (index - 1 + slides.length) % slides.length;
-		    slides[index].classList.add('active');
+		function prevSlide() {
+			slides[index].classList.remove('active');
+			index = (index - 1 + slides.length) % slides.length;
+			slides[index].classList.add('active');
 		}
 	</script>
 	<?php include 'components/alert.php'; ?>
 </body>
+
 </html>
