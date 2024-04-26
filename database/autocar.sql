a77e6985ae2106f18064ac4bb1c444c447ecef16
Hillel3009 <161851232+Hillel3009@users.noreply.github.com>
2024-04-26 16:10:57 +0200

    modif bdd

--- database/autocar.sql
+++ database/autocar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 26 avr. 2024 à 03:21
+-- Généré le : ven. 26 avr. 2024 à 15:51
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -81,23 +81,21 @@ INSERT INTO `cart` (`id`, `user_id`, `product_id`, `price`, `qty`) VALUES
 --
 
 CREATE TABLE `message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` varchar(255) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(100) NOT NULL,
   `subject` varchar(255) NOT NULL,
-  `message` text NOT NULL,
-  `phone` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `message` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `message`
 --
 
-INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `subject`, `message`, `phone`) VALUES
-(1, '0', 'Paul', 'paul.roux@gmail.com', 'shop', 'good', '+123456789'),
-(2, 'UAVjN46f0bvXSKquej8S', 'Paul', 'paul.roux@gmail.com', 'maths,science', 'kk', '+987654321');
+INSERT INTO `message` (`id`, `user_id`, `name`, `email`, `subject`, `message`) VALUES
+('0', '0', 'mahi', 'mahinazir@gmail.com', 'shop', 'good'),
+('Lm7uFQVcX3czwG0yX5p0', 'UAVjN46f0bvXSKquej8S', 'mahi', 'mahinazir@gmail.com', 'maths,science', 'kk');
 
 -- --------------------------------------------------------
 
@@ -142,6 +140,7 @@ INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `address`, `ad
 ('JqyfHoT9UzR4qcvp3LNJ', 'd5URvsP8VusCXQoCdMBG', 'Camille', '0675001234', 'camille.blanchard@gmail.com', '20 rue Montorgueil, 75001 Paris', 'home', 'credit or debit card', 'BLTtlhOgq1cuz7plh4Ia', '123', '1', '2023-02-28', 'en cours', 'completee'),
 ('yyD4B276Pg9lfGpRjcr9', 'd5URvsP8VusCXQoCdMBG', 'Camille', '0675045678', 'camille.blanchard@gmail.com', '20 rue Montorgueil, 75001 Paris', 'office', 'credit or debit card', 'jo35YMmBWpvbCMB65UdA', '160', '2', '2023-02-28', 'annulee', 'en attente');
 
+
 -- --------------------------------------------------------
 
 --
@@ -171,18 +170,15 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `image`, `image3`, `image2`, `product_detail`, `status`, `Modèle`, `Année`, `Moteur`, `Kilométrage`, `Equipements`, `Etat`, `Pointsforts`) VALUES
-('BLTtlhOgq1cuz7plh4Ia', 'McLaren 720s', '375 000', 'mc1.jpg', 'mc3.png', 'mc2.jpg', 'Véhicule sportif haute performance alliant design aérodynamique et technologie de pointe.', 'actif', 'McLaren 720s', '2022', 'V8 de 4,0 L', '1000 Km', 'Climatisation automatique bizone, Régulateur de vitesse, Système multimédia avec écran tactile de 8 pouces, Bluetooth, Prises USB, Navigateur GPS, Capteurs de pluie et de lumière', 'Neuf', 'Le 0 à 100 km/h est parcouru en seulement 2,8 secondes'),
-('jo35YMmBWpvbCMB65UdA', 'Bugatti La Voiture Noire', '14 999 998', 'bugatienoir.jpg', 'bugati3.jpg', 'bugati2.jpg', 'Un chef-d’œuvre moderne rendant hommage au patrimoine de Bugatti, limité à quelques unités.', 'actif', 'Bugatti La Voiture Noire', '2021', '16-cylindres en W quadri-turbo de 8 litres', '0 Km', 'Climatisation automatique bizone, système multimédia avancé, sièges en cuir premium, capteurs de stationnement.', 'Neuf', '0 à 100 km/h en 2,4s'),
-('aSBHDzG26iXurm6cfoNv', 'Lamborghini Revuelto', '699 000', 'lambo4.jpg', 'lambo6.jpg', 'lambo5.jpg', 'Un supercar électrifiant qui combine performance extrême et style audacieux.', 'actif', 'Lamborghini Revuelto', '2023', 'V12 hybride', '75 km', 'Système infotainment avec écran tactile, intégration smartphone, régulateur de vitesse adaptatif.', 'Neuf', 'Une accélération de 0 à 100 km/h en seulement 2,8 secondes.'),
-('g5DLcNHmtHvq3DtJYsCb', 'Alpine A110 R', '111 997', 'alpine.jpg', 'alpine3.jpg', 'alpine2.jpg', 'Voiture sportive légère et agile, optimisée pour les performances de piste.', 'actif', 'Alpine A110 R', '2020', '4 cylindres en ligne de 1,8 L', '1000 Km', 'KIT AERO MP-R', 'Occasion', '0 à 100 km/h en 3,9 sec pour une vitesse de pointe de 285 km/h'),
-('uOarNNg0n3KD9OvPtItP', 'Ferrari SP51', '10 000 000', 'ferariesp51.png', 'ferarie3.jpg', 'ferarie2.png', 'Un roadster exclusif au design saisissant et aux performances époustouflantes, basé sur la 812 GTS.', 'actif', 'Ferrari SP51', '2022', 'V12', '0 Km', 'Intérieur luxueux en cuir, système de navigation avancé, caméra de recul.', 'Neuf', '0 à 100 km/h en moins de 3 secondes'),
-('26lPPTjXh9EkNc7WocS5', 'Porsche 911 GT3 R', '253 454', 'porche.png', 'porche3.jpg', 'porche2.png', 'Conçue pour la compétition, cette Porsche offre des performances de pointe sur piste et sur route.', 'actif', 'Porsche 911 GT3 R', '2022', '4,0 L Flat-6', '80 km', 'Système de suspension adaptative, freins haute performance, cockpit orienté course.', 'Neuf', 'Excellente dynamique de conduite, puissance impressionnante.'),
-('kun96OpQed6Eww6M1URo', 'Lamborghini Urus', '220 000', 'urus1.jpg', 'urus3.jpg', 'urus2.jpg', 'Un SUV de luxe qui combine la performance d’une supercar avec la praticité d’un véhicule familial.', 'actif', 'Lamborghini Urus', '2018', 'V8 biturbo de 4,0 L', '50 km', 'Interface multimédia avancée, sièges en cuir avec réglages électriques, caméras de stationnement, système audio premium.', 'Neuf', 'Polyvalence impressionnante, performances hors normes pour un SUV.'),
-('bEvIK2PvwOqY4l8nuPTZ', 'Mercedes-AMG GT3 Edition 55', '625 000', 'mercedes.jpg', 'mercedes5.jpg', 'mercedes4.jpg', 'Une édition spéciale marquant le 55e anniversaire d’AMG, avec des améliorations axées sur la performance et l’exclusivité.', 'actif', 'Mercedes-AMG GT3 Edition 55', '2022', 'V8 de 4,0 L turbo', '100 km', 'Cockpit de course, système de sécurité intégré, châssis et suspension sport.', 'Neuf', 'Édition limitée, optimisée pour la compétition et la conduite haute performance.'),
-('eBbtkVNYiJJKT9mCgYbk', 'Maserati Granturismo', '180 000', 'maserati1.jpg', 'maserati3.jpg', 'maserati2.jpg', 'Un coupé élégant qui allie confort de luxe à des performances sportives, idéal pour le grand tourisme.', 'actif', 'Maserati Granturismo', '2017', 'V6 de 3,0 L turbo', '85 km', 'Intérieur cuir, système multimédia avec écran tactile, régulateur de vitesse adaptatif, toit ouvrant.', 'Neuf', 'Confort supérieur, design intemporel.'),
-('rfA9q4uWC2JvzLCRmawT', 'Aston Martin Vantage GT3', '400 000', 'aston.jpg', 'aston3.jpg', 'aston2.jpg', 'Une voiture de course homologuée pour la route, offrant des performances extrêmes et un design agressif.', 'actif', 'Aston Martin Vantage GT3', '2023', 'V8 de 4,0 L turbo', '70 km', 'Système de navigation avancé, sièges sport en cuir, suspension sportive, aérodynamique optimisée.', 'Neuf', 'Capacités de piste exceptionnelles, accélération rapide.'),
-('0I8ZbLUgrxn7qWNMzxPE', 'McLaren 720s GT3 X', '330 000', 'MC3.jpg', 'MC5.jpg', 'MC4.jpg', 'Version extrême du 720s, conçue exclusivement pour une utilisation sur circuit avec des améliorations significatives en termes de performance.', 'actif', 'McLaren 720s GT3 X', '2021', 'V8 de 4,0 L turbo', '40 km', 'Cockpit de course, système de télémétrie avancé, freins en carbone-céramique.', 'Neuf', 'Performance de circuit dominante, très limitée en production.'),
-('wrJTrzoHsuEwr7hGi3R6', 'Mercedes AMG GT2', '300 000', 'mercedes1.jpg', 'mercedes3.png', 'mercedes2.jpg', 'Voiture de sport axée sur la performance avec un design élégant et une ingénierie de précision.', 'actif', 'Mercedes AMG GT2', '2023', 'V8 de 4,0 L turbo', '55 km', 'Sièges sport AMG, système multimédia haut de gamme, aide à la conduite active.', 'Neuf', 'Équilibre parfait entre performance et confort, design agressif.');
+('jo35YMmBWpvbCMB65UdA', 'Bugatti la voiture noir', '14999998', 'bugatienoir.jpg', 'bugati3.jpg', 'bugati2.jpg', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'Bugatti la voiture noir', '2021', '16-cylindres en W quadri-turbo de 8 litres', '0 Km', '/', 'Neuf', '0 à 100 km/h en 2,4s'),
+('aSBHDzG26iXurm6cfoNv', 'Lamborghini Revuelto', '699000', 'lambo4.jpg', 'lambo6.jpg', 'lambo5.jpg', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'Lamborghini Revuelto', '2023', 'V12 de 6,5 L', '5000 Km', 'ÉQUIPEMENT DE CHARGE, PROTECTION EXTÉRIEURE, FILM DE PROTECTION, PROTECTION INTÉRIEURE', 'Occasion', '0 à 100 km/h en 2,5 s'),
+('g5DLcNHmtHvq3DtJYsCb', 'Alpine A110 R', '111997', 'alpine.jpg', 'alpine3.jpg', 'alpine2.jpg', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'Alpine A110 R', '2020', '4 cylindres en ligne de 1,8 L', '1000 Km', 'KIT AERO MP-R', 'Occasion', '0 à 100 km/h en 3,9 sec pour une vitesse de pointe de 285 km/h'),
+('uOarNNg0n3KD9OvPtItP', 'Ferrari SP51', '10000000', 'ferariesp51.png', 'ferarie3.jpg', 'ferarie2.png', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'Ferrari SP51', '2022', 'V12', '0 Km', '/', 'Neuf', '0 à 100 km/h en moins de 3 secondes'),
+('26lPPTjXh9EkNc7WocS5', 'Porsche 911 GT3 R', '247996', 'porche.png', 'porche3.jpg', 'porche2.png', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'Porsche 911 GT3 R', '2020', 'V8 de 4 L', '9547 Km', 'Série de sièges baquets, Carbone apparent, inserts en carbone, Pack Chrono, Assistant de passage des vitesses', 'Occasion', '0 à 100 km/h en 3,2 s'),
+('kun96OpQed6Eww6M1URo', 'LAMBORGHINI URUS', '214999', 'urus1.jpg', 'urus3.jpg', 'urus2.jpg', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'LAMBORGHINI URUS', ' 2023', 'V8 de 4,0 L', '0 Km', 'VOYAGE, PET CARE, ÉCHAPPEMENT EN TITANE, COMPOSANTS EN FIBRE DE CARBONE, ACCESSOIRES D’HIVER', 'Neuve', '0 à 100 km/h en 3,6 s'),
+('wrJTrzoHsuEwr7hGi3R6', 'Mercedes-AMG GT3 Edition 55', '625000', 'mercedes1.jpg', 'mercedes3.png', 'mercedes2.jpg', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit am', 'actif', 'Mercedes-AMG GT3 Edition 55', '2022', 'V8 de 6,3 L', '1000 Km', 'Le pack AMG Line', 'Occasion', '0 à 100 km/h en 3,2 s'),
+('eBbtkVNYiJJKT9mCgYbk', 'Maserati Granturismo', '181395', 'maserati1.jpg', 'maserati3.jpg', 'maserati2.jpg', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adip', 'actif', 'Maserati Granturismo', '2019', 'v6 de 3,0 L', '12 000 Km', 'Entretien et protection, Roues équilibrées, Accessoires d’entretien et de protection,', 'Occasion', '0 à 100 km/h en 3,5 s'),
+('Hs4tKXIJkieL1I0tmN9H', 'maclaren 720s', '374998', 'mc1.jpg', 'mc3.png', 'mc2.jpg', 'DSDSDSD', 'actif', 'maclaren 720s', '2022', 'V8 de 4,0 L', '0 Km', 'Deux airbags, Climatisation automatique bizone, Régulateur de vitesse, Système multimédia avec écran tactile de 8 pouces, Capteurs de pluie et de lumière', 'Neuve', '0 à 100 km/h en 2,9 s');
 
 -- --------------------------------------------------------
 
@@ -207,7 +203,6 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 ('ir7qjxTxaQm9PM5drpEn', 'Sophie', 'sophie.moreau@gmail.com', 'sophie786', 'user'),
 ('GE2LLAWjKATiQRLHaa6O', 'Maxime', 'maxime.petit@gmail.com', '12345', 'user'),
 ('d5URvsP8VusCXQoCdMBG', 'Camille', 'camille.blanchard@gmail.com', '12345', 'user');
-
 -- --------------------------------------------------------
 
 --
@@ -227,7 +222,6 @@ CREATE TABLE `wishlist` (
 
 INSERT INTO `wishlist` (`id`, `user_id`, `product_id`, `price`) VALUES
 ('FngaVJmk3vBeq3KUmt03', 'UAVjN46f0bvXSKquej8S', 'jo35YMmBWpvbCMB65UdA', '160');
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
