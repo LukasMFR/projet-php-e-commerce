862bd5125044ee02e722a3d007872bbf35e18f75
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-04-28 15:58:33 +0200

    Fix SQL database, message and Bugatti car

--- database/autocar-lukas.sql
+++ database/autocar-lukas.sql
@@ -3,9 +3,10 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 26 avr. 2024 à 03:21
+-- Généré le : dim. 28 avr. 2024 à 15:52
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
+
 
 DROP DATABASE IF EXISTS autocar;
 CREATE DATABASE autocar ;
@@ -159,20 +160,20 @@ CREATE TABLE `products` (
   `status` varchar(100) NOT NULL,
   `Modèle` varchar(255) NOT NULL,
   `Année` varchar(255) NOT NULL,
-  `Moteur` varchar(255) NOT NULL,
-  `Kilométrage` varchar(255) NOT NULL,
-  `Equipements` varchar(255) NOT NULL,
-  `Etat` varchar(255) NOT NULL,
-  `Pointsforts` varchar(255) NOT NULL
+  `moteur` varchar(255) NOT NULL,
+  `kilométrage` varchar(255) NOT NULL,
+  `equipements` varchar(255) NOT NULL,
+  `etat` varchar(255) NOT NULL,
+  `pointsforts` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `products`
 --
 
-INSERT INTO `products` (`id`, `name`, `price`, `image`, `image3`, `image2`, `product_detail`, `status`, `Modèle`, `Année`, `Moteur`, `Kilométrage`, `Equipements`, `Etat`, `Pointsforts`) VALUES
+INSERT INTO `products` (`id`, `name`, `price`, `image`, `image3`, `image2`, `product_detail`, `status`, `Modèle`, `Année`, `moteur`, `kilométrage`, `equipements`, `etat`, `pointsforts`) VALUES
 ('BLTtlhOgq1cuz7plh4Ia', 'McLaren 720s', '375 000', 'mc1.jpg', 'mc3.png', 'mc2.jpg', 'Véhicule sportif haute performance alliant design aérodynamique et technologie de pointe.', 'actif', 'McLaren 720s', '2022', 'V8 de 4,0 L', '1000 Km', 'Climatisation automatique bizone, Régulateur de vitesse, Système multimédia avec écran tactile de 8 pouces, Bluetooth, Prises USB, Navigateur GPS, Capteurs de pluie et de lumière', 'Neuf', 'Le 0 à 100 km/h est parcouru en seulement 2,8 secondes'),
-('jo35YMmBWpvbCMB65UdA', 'Bugatti La Voiture Noire', '14 999 998', 'bugatienoir.jpg', 'bugati3.jpg', 'bugati2.jpg', 'Un chef-d’œuvre moderne rendant hommage au patrimoine de Bugatti, limité à quelques unités.', 'actif', 'Bugatti La Voiture Noire', '2021', '16-cylindres en W quadri-turbo de 8 litres', '0 Km', 'Climatisation automatique bizone, système multimédia avancé, sièges en cuir premium, capteurs de stationnement.', 'Neuf', '0 à 100 km/h en 2,4s'),
+('jo35YMmBWpvbCMB65UdA', 'Bugatti La Voiture Noire', '10 000 000', 'bugatienoir.jpg', 'bugati3.jpg', 'bugati2.jpg', 'Un chef-d’œuvre moderne rendant hommage au patrimoine de Bugatti, limité à quelques unités.', 'actif', 'Bugatti La Voiture Noire', '2029', '16-cylindres en W quadri-turbo de 8 litres', '10 Km', 'Climatisation automatique bizone, système multimédia avancé, sièges en cuir premium, capteurs de stationnement.', 'Neuf', '0 à 100 km/h en 2,4s'),
 ('aSBHDzG26iXurm6cfoNv', 'Lamborghini Revuelto', '699 000', 'lambo4.jpg', 'lambo6.jpg', 'lambo5.jpg', 'Un supercar électrifiant qui combine performance extrême et style audacieux.', 'actif', 'Lamborghini Revuelto', '2023', 'V12 hybride', '75 km', 'Système infotainment avec écran tactile, intégration smartphone, régulateur de vitesse adaptatif.', 'Neuf', 'Une accélération de 0 à 100 km/h en seulement 2,8 secondes.'),
 ('g5DLcNHmtHvq3DtJYsCb', 'Alpine A110 R', '111 997', 'alpine.jpg', 'alpine3.jpg', 'alpine2.jpg', 'Voiture sportive légère et agile, optimisée pour les performances de piste.', 'actif', 'Alpine A110 R', '2020', '4 cylindres en ligne de 1,8 L', '1000 Km', 'KIT AERO MP-R', 'Occasion', '0 à 100 km/h en 3,9 sec pour une vitesse de pointe de 285 km/h'),
 ('uOarNNg0n3KD9OvPtItP', 'Ferrari SP51', '10 000 000', 'ferariesp51.png', 'ferarie3.jpg', 'ferarie2.png', 'Un roadster exclusif au design saisissant et aux performances époustouflantes, basé sur la 812 GTS.', 'actif', 'Ferrari SP51', '2022', 'V12', '0 Km', 'Intérieur luxueux en cuir, système de navigation avancé, caméra de recul.', 'Neuf', '0 à 100 km/h en moins de 3 secondes'),
@@ -183,6 +184,36 @@ INSERT INTO `products` (`id`, `name`, `price`, `image`, `image3`, `image2`, `pro
 ('rfA9q4uWC2JvzLCRmawT', 'Aston Martin Vantage GT3', '400 000', 'aston.jpg', 'aston3.jpg', 'aston2.jpg', 'Une voiture de course homologuée pour la route, offrant des performances extrêmes et un design agressif.', 'inactif', 'Aston Martin Vantage GT3', '2023', 'V8 de 4,0 L turbo', '70 km', 'Système de navigation avancé, sièges sport en cuir, suspension sportive, aérodynamique optimisée.', 'Neuf', 'Capacités de piste exceptionnelles, accélération rapide.'),
 ('0I8ZbLUgrxn7qWNMzxPE', 'McLaren 720s GT3 X', '330 000', 'MC3.jpg', 'MC5.jpg', 'MC4.jpg', 'Version extrême du 720s, conçue exclusivement pour une utilisation sur circuit avec des améliorations significatives en termes de performance.', 'inactif', 'McLaren 720s GT3 X', '2021', 'V8 de 4,0 L turbo', '40 km', 'Cockpit de course, système de télémétrie avancé, freins en carbone-céramique.', 'Neuf', 'Performance de circuit dominante, très limitée en production.'),
 ('wrJTrzoHsuEwr7hGi3R6', 'Mercedes AMG GT2', '300 000', 'mercedes1.jpg', 'mercedes3.png', 'mercedes2.jpg', 'Voiture de sport axée sur la performance avec un design élégant et une ingénierie de précision.', 'actif', 'Mercedes AMG GT2', '2023', 'V8 de 4,0 L turbo', '55 km', 'Sièges sport AMG, système multimédia haut de gamme, aide à la conduite active.', 'Neuf', 'Équilibre parfait entre performance et confort, design agressif.');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `puff`
+--
+
+CREATE TABLE `puff` (
+  `id` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `product_detail` varchar(500) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `goût` varchar(255) NOT NULL,
+  `taffe` varchar(255) NOT NULL,
+  `nicotine` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `puff`
+--
+
+INSERT INTO `puff` (`id`, `name`, `price`, `image`, `product_detail`, `status`, `goût`, `taffe`, `nicotine`) VALUES
+('wrJTrzoHsuEwr7hGi3R6', 'Puff naturel', '300 ', 'puff6.jpg', 'A la recherche d’une saveur sucrée et originale pour votre vapotage ? Découvrez la TurboVape Gaz Naturelle by Road Luxury, aux arômes exclusifs pour une vape fun et gourmande ! ', 'actif', 'Gaz Naturelle', '1000', 0),
+('pWY5Mf0987NUdbPoxuBu', 'Puff Gazole', '12', 'puff1.jpg', 'A la recherche d’une saveur sucrée et originale pour votre vapotage ? Découvrez la TurboVape Gazole by Road Luxury, aux arômes exclusifs pour une vape fun et gourmande ! ', 'actif', 'Gazole', '1000', 0),
+('ICg3StZandimDp1BU1m2', 'Puff Sans Plomb 95', '14', 'puff2.jpg', 'A la recherche d’une saveur sucrée et originale pour votre vapotage ? Découvrez la TurboVape Sans Plomb 95 by Road Luxury, aux arômes exclusifs pour une vape fun et gourmande ! ', 'actif', 'Sans Plomb 95', '1000', 0),
+('0lKHBlsFPaTpTFlnHUXf', 'Puff Superéthanol E85', '12', 'puff3.jpg', 'A la recherche d’une saveur sucrée et originale pour votre vapotage ? Découvrez la TurboVape Superéthanol E85 by Road Luxury, aux arômes exclusifs pour une vape fun et gourmande ! ', 'actif', 'Superéthanol E85', '1000', 0),
+('Wd3or2BwCtKebztba6aV', 'Puff Supercarburants SP95', '12', 'puff4.jpg', 'A la recherche d’une saveur sucrée et originale pour votre vapotage ? Découvrez la TurboVape Supercarburants SP95 by Road Luxury, aux arômes exclusifs pour une vape fun et gourmande ! ', 'actif', 'Supercarburants SP95', '1000', 0),
+('ugVkMKuH66eqSJFRyhAU', 'Puff électrique', '12', 'puff5.jpg', 'A la recherche d’une saveur sucrée et originale pour votre vapotage ? Découvrez la TurboVape Carburant Electrique by Road Luxury, aux arômes exclusifs pour une vape fun et gourmande ! ', 'actif', 'Electrique', '1000', 0);
 
 -- --------------------------------------------------------
 
