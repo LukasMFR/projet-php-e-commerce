4c2bd5860fec309bb4b9e9d6004338063ae4ca9e
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-04-29 14:03:13 +0200

    Add puff_id wishlit

--- database/autocar.sql
+++ database/autocar.sql
@@ -247,6 +247,7 @@ CREATE TABLE `wishlist` (
   `id` varchar(255) NOT NULL,
   `user_id` varchar(255) NOT NULL,
   `product_id` varchar(255) NOT NULL,
+  `puff_id` varchar(255) NOT NULL,
   `price` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
