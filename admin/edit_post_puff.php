979461715f5ce649435ff92a44319278a8dad8ea
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-01 12:41:03 +0200

    Update top part of edit_post_puff.php

--- admin/edit_post_puff.php
+++ admin/edit_post_puff.php
@@ -10,27 +10,13 @@ if (!isset($admin_id)) {
 
 if (isset($_POST['save'])) {
 	$post_id = $_GET['id'];
-	$title = $_POST['title'];
-	$title = filter_var($title, FILTER_SANITIZE_STRING);
-	$price = $_POST['price'];
-	$price = filter_var($price, FILTER_SANITIZE_STRING);
-	$content = $_POST['content'];
-	$content = filter_var($content, FILTER_SANITIZE_STRING);
-	$status = $_POST['status'];
-	$status = filter_var($status, FILTER_SANITIZE_STRING);
-	$goût = $_POST['goût'];
-	$goût = filter_var($goût, FILTER_SANITIZE_STRING);
-	$taffe = $_POST['taffe'];
-	$taffe = filter_var($taffe, FILTER_SANITIZE_STRING);
-	$nicotine = $_POST['nicotine'];
-	$nicotine = filter_var($nicotine, FILTER_SANITIZE_STRING);
-
-
-
-	$update_post = $conn->prepare("UPDATE `puff` SET name = ?, price = ?, product_detail = ?,status = ?, goût = ?, taffe = ?, nicotine = ? WHERE id = ?");
-	$update_post->execute([$title, $price, $content, $status, $goût, $taffe, $nicotine, $post_id]);
-
-	$success_msg[] = 'Produit mis à jour';
+	$name = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+	$price = filter_var($_POST['price'], FILTER_SANITIZE_STRING);
+	$detail = filter_var($_POST['content'], FILTER_SANITIZE_STRING);
+	$status = filter_var($_POST['status'], FILTER_SANITIZE_STRING);
+	$goût = filter_var($_POST['goût'], FILTER_SANITIZE_STRING);
+	$taffe = filter_var($_POST['taffe'], FILTER_SANITIZE_STRING);
+	$nicotine = filter_var($_POST['nicotine'], FILTER_SANITIZE_STRING);
 
 	$old_image = $_POST['old_image'];
 	$image = $_FILES['image']['name'];
@@ -38,65 +24,59 @@ if (isset($_POST['save'])) {
 	$image_tmp_name = $_FILES['image']['tmp_name'];
 	$image_folder = '../image/' . $image;
 
-
-	$select_image = $conn->prepare("SELECT * FROM `puff` WHERE image = ?");
-	$select_image->execute([$image]);
-
 	if (!empty($image)) {
 		if ($image_size > 2000000) {
 			$warning_msg[] = 'La taille de l\'image est trop grande';
-		} elseif ($select_image->rowCount() > 0 and $image != '') {
-			$warning_msg[] = 'Veuillez renommer votre image';
 		} else {
-			$update_image = $conn->prepare("UPDATE `puff` SET image = ? WHERE id = ?");
-			$update_image->execute([$image, $post_id]);
-			move_uploaded_file($image_tmp_name, $image_folder);
-			if ($old_image != $image and $old_image != '') {
+			if ($old_image != $image) {
+				$update_image = $conn->prepare("UPDATE `puff` SET image = ? WHERE id = ?");
+				$update_image->execute([$image, $post_id]);
+				move_uploaded_file($image_tmp_name, $image_folder);
 				unlink('../image/' . $old_image);
+				$success_msg[] = 'Image mise à jour !';
 			}
-			$success_msg[] = 'Image mise à jour !';
 		}
 	}
 
-	
-} 
-
-
-	
-//delete post
-if (isset($_POST['delete_post'])) {
-
-	$post_id = $_POST['post_id'];
-	$post_id = filter_var($post_id, FILTER_SANITIZE_STRING);
-	$delete_image = $conn->prepare("SELECT * FROM `puff` WHERE id = ?");
-	$delete_image->execute([$post_id]);
-	$fetch_delete_image = $delete_image->fetch(PDO::FETCH_ASSOC);
-	if ($fetch_delete_image['image'] != '') {
-		unlink('../image/' . $fetch_delete_image['image']);
-	}
-	$delete_post = $conn->prepare("DELETE FROM `puff` WHERE id = ?");
-	$delete_post->execute([$post_id]);
-	$delete_comments = $conn->prepare("DELETE FROM `comments` WHERE post_id = ?");
-	$delete_comments->execute([$post_id]);
-	$success_msg[] = 'Produit supprimé avec succès !';
-
+	$update_post = $conn->prepare("UPDATE `puff` SET name = ?, price = ?, product_detail = ?, status = ?, goût = ?, taffe = ?, nicotine = ? WHERE id = ?");
+	$update_post->execute([$name, $price, $detail, $status, $goût, $taffe, $nicotine, $post_id]);
+	$success_msg[] = 'Produit mis à jour';
 }
 
-//delete image 
-
-if (isset($_POST['delete_image'])) {
-	$empty_image = '';
+// Suppression de produit
+if (isset($_POST['delete_post'])) {
 	$post_id = $_POST['post_id'];
-	$post_id = filter_var($post_id, FILTER_SANITIZE_STRING);
-	$delete_image = $conn->prepare("SELECT * FROM `puff` WHERE id = ?");
-	$delete_image->execute([$post_id]);
-	$fetch_delete_image = $delete_image->fetch(PDO::FETCH_ASSOC);
-	if ($fetch_delete_image['image'] != '') {
-		unlink('../image/' . $fetch_delete_image['image']);
+
+	// Suppression de l'image associée
+	$select_image = $conn->prepare("SELECT image FROM `puff` WHERE id = ?");
+	$select_image->execute([$post_id]);
+	if ($fetch_image = $select_image->fetch()) {
+		$image_path = '../image/' . $fetch_image['image'];
+		if (file_exists($image_path)) {
+			unlink($image_path);
+		}
 	}
-	$unset_image = $conn->prepare("UPDATE `puff` SET image = ? WHERE id=?");
-	$unset_image->execute([$empty_image, $post_id]);
-	$success_msg[] = 'Image supprimée avec succès';
+
+	// Suppression du produit
+	$delete_post = $conn->prepare("DELETE FROM `puff` WHERE id = ?");
+	$delete_post->execute([$post_id]);
+	$success_msg[] = 'Produit supprimé avec succès !';
+}
+
+// Suppression de l'image uniquement
+if (isset($_POST['delete_image'])) {
+	$post_id = $_POST['post_id'];
+	$select_image = $conn->prepare("SELECT image FROM `puff` WHERE id = ?");
+	$select_image->execute([$post_id]);
+	if ($fetch_image = $select_image->fetch()) {
+		$image_path = '../image/' . $fetch_image['image'];
+		if (file_exists($image_path)) {
+			unlink($image_path);
+		}
+		$update_image = $conn->prepare("UPDATE `puff` SET image = '' WHERE id = ?");
+		$update_image->execute([$post_id]);
+		$success_msg[] = 'Image supprimée avec succès';
+	}
 }
 ?>
 <style>
@@ -212,9 +192,8 @@ if (isset($_POST['delete_image'])) {
 
 					<?php
 				}
-			
-		}
-			else {
+
+			} else {
 
 				echo '
 							<div class="empty">
