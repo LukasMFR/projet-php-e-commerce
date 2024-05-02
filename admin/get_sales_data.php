b095c527033f6b1e1cf3373f9e5f65d40ffb5cd4
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-02 17:53:04 +0200

    Update SQL query on get_sales_data.php

--- admin/get_sales_data.php
+++ admin/get_sales_data.php
@@ -1,11 +1,15 @@
 <?php
-include '../components/connection.php';  
+include '../components/connection.php';
 
-$query = "SELECT p.name as product_name, SUM(o.qty) as total_quantity FROM orders o JOIN products p ON o.product_id = p.id GROUP BY o.product_id";
+// Requête mise à jour pour intégrer item_id et item_type
+$query = "SELECT p.name as product_name, SUM(o.qty) as total_quantity 
+          FROM orders o 
+          JOIN products p ON o.item_id = p.id AND o.item_type = 'product' 
+          GROUP BY o.item_id";
+
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($results);
 ?>
-
