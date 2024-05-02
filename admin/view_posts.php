811ba7cb9e46ec6e53eb6324c15c5b7e3216079a
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-02 18:19:17 +0200

    Fix big numbers showing up as phone view_posts.php

--- admin/view_posts.php
+++ admin/view_posts.php
@@ -39,6 +39,7 @@ if (isset($_POST['delete'])) {
 	<link rel="icon" type="image/png" href="../img/favicon-64.png">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 	<title>Les produits - Road Luxury</title>
+	<?php include '../components/meta_tags.php'; ?>
 </head>
 
 <body>
