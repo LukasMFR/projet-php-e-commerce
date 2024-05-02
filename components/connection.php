7e11ca2b7eb29356791c94a2ee39be0a3077509c
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-02 22:10:15 +0200

    Use try catch when connecting to DB for good practice

--- components/connection.php
+++ components/connection.php
@@ -1,14 +1,40 @@
 <?php
-// Code utilsé pour afficher les erreurs et les debugs
+// Code utilisé pour afficher les erreurs et les debugs
 // ini_set('display_errors', 1);
 // error_reporting(E_ALL);
-$db_name = 'mysql:host=localhost;dbname=autocar';
+
+// Informations de connexion à la base de données
+// $dsn = 'mysql:host=localhost;dbname=autocar;charset=utf8mb4';
+$dsn = 'mysql:host=localhost;dbname=autocar';
 $db_user = 'root';
 $db_password = '';
 
-$conn = new PDO($db_name, $db_user, $db_password);
+try {
+	$conn = new PDO($dsn, $db_user, $db_password);
+	// Définit le mode d'erreur pour générer des exceptions
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	// Afficher les erreurs de connexion à la base de données
+	die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
 
+// Tableau de traduction des mois
+$mois = [
+    'January' => 'janvier',
+    'February' => 'février',
+    'March' => 'mars',
+    'April' => 'avril',
+    'May' => 'mai',
+    'June' => 'juin',
+    'July' => 'juillet',
+    'August' => 'août',
+    'September' => 'septembre',
+    'October' => 'octobre',
+    'November' => 'novembre',
+    'December' => 'décembre'
+];
 
+// Fonction pour générer un ID unique
 function unique_id()
 {
 	$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
