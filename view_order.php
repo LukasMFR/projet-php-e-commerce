104ba0e9ac310d2c487babcf874b1a0750b76d43
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-04 00:22:47 +0200

    Add back box-container tab view order page just in case

--- view_order.php
+++ view_order.php
@@ -59,72 +59,72 @@ if (isset($_POST['cancel'])) {
 				<p>Retrouvez ici tous les détails de votre commande, y compris les informations sur le produit, le
 					montant total et le statut de livraison.</p>
 			</div>
-			<!-- <div class="box-container"> -->
-			<?php
-			$select_orders = $conn->prepare("SELECT * FROM `orders` WHERE id=? LIMIT 1");
-			$select_orders->execute([$get_id]);
-			if ($select_orders->rowCount() > 0) {
-				while ($fetch_order = $select_orders->fetch(PDO::FETCH_ASSOC)) {
-					$table = ($fetch_order['item_type'] === 'puff') ? 'puff' : 'products';
-					$select_item = $conn->prepare("SELECT * FROM `$table` WHERE id=? LIMIT 1");
-					$select_item->execute([$fetch_order['item_id']]);
+			<div class="box-container">
+				<?php
+				$select_orders = $conn->prepare("SELECT * FROM `orders` WHERE id=? LIMIT 1");
+				$select_orders->execute([$get_id]);
+				if ($select_orders->rowCount() > 0) {
+					while ($fetch_order = $select_orders->fetch(PDO::FETCH_ASSOC)) {
+						$table = ($fetch_order['item_type'] === 'puff') ? 'puff' : 'products';
+						$select_item = $conn->prepare("SELECT * FROM `$table` WHERE id=? LIMIT 1");
+						$select_item->execute([$fetch_order['item_id']]);
 
-					if ($select_item->rowCount() > 0) {
-						while ($fetch_item = $select_item->fetch(PDO::FETCH_ASSOC)) {
-							$sub_total = ($fetch_order['price'] * $fetch_order['qty']);
-							?>
-							<div class="box">
-								<div class="product-details">
-									<p class="date"><i class="bi bi-calendar-fill"></i>
-										<?php
-										$date = strtotime($fetch_order['date']);
-										$formattedDate = date('d ', $date) . $mois[date('F', $date)] . date(' Y à H:i', $date);
-										echo $formattedDate;
-										?>
-									</p>
-									<img src="image/<?= $fetch_item['image']; ?>" class="image">
-									<div class="product-info">
-										<p class="price"><?= number_format($fetch_item['price'], 2, ',', ' '); ?> €</p>
-										<p class="quantity">Quantité : <?= $fetch_order['qty']; ?></p>
-										<h3 class="name"><?= $fetch_item['name']; ?></h3>
-										<p class="grand-total">Montant total : <span><?= number_format($sub_total, 2, ',', ' '); ?>
-												€</span></p>
-									</div>
-								</div>
-								<div class="billing-info">
-									<p class="title">Adresse de facturation</p>
-									<div class="user-details">
-										<p class="user"><i class="bi bi-person-bounding-box"></i><?= $fetch_order['name']; ?></p>
-										<p class="user"><i class="bi bi-phone"></i><?= $fetch_order['number']; ?></p>
-										<p class="user"><i class="bi bi-envelope"></i><?= $fetch_order['email']; ?></p>
-										<p class="user"><i class="bi bi-pin-map-fill"></i><?= $fetch_order['address']; ?></p>
-										<p class="status">Statut : <span
-												data-status="<?= strtolower($fetch_order['status']); ?>"><?= $fetch_order['status']; ?></span>
+						if ($select_item->rowCount() > 0) {
+							while ($fetch_item = $select_item->fetch(PDO::FETCH_ASSOC)) {
+								$sub_total = ($fetch_order['price'] * $fetch_order['qty']);
+								?>
+								<div class="box">
+									<div class="product-details">
+										<p class="date"><i class="bi bi-calendar-fill"></i>
+											<?php
+											$date = strtotime($fetch_order['date']);
+											$formattedDate = date('d ', $date) . $mois[date('F', $date)] . date(' Y à H:i', $date);
+											echo $formattedDate;
+											?>
 										</p>
+										<img src="image/<?= $fetch_item['image']; ?>" class="image">
+										<div class="product-info">
+											<p class="price"><?= number_format($fetch_item['price'], 2, ',', ' '); ?> €</p>
+											<p class="quantity">Quantité : <?= $fetch_order['qty']; ?></p>
+											<h3 class="name"><?= $fetch_item['name']; ?></h3>
+											<p class="grand-total">Montant total : <span><?= number_format($sub_total, 2, ',', ' '); ?>
+													€</span></p>
+										</div>
 									</div>
-									<?php if ($fetch_order['status'] == 'annulee') { ?>
-										<a href="checkout.php?get_id=<?= $fetch_item['id']; ?>&type=<?= $fetch_order['item_type']; ?>"
-											class="btn">Commander à nouveau</a>
-									<?php } else { ?>
-										<form method="post">
-											<button type="submit" name="cancel" class="btn"
-												onclick="return confirm('Voulez-vous annuler cette commande ?')">Annuler la
-												commande</button>
-										</form>
-									<?php } ?>
+									<div class="billing-info">
+										<p class="title">Adresse de facturation</p>
+										<div class="user-details">
+											<p class="user"><i class="bi bi-person-bounding-box"></i><?= $fetch_order['name']; ?></p>
+											<p class="user"><i class="bi bi-phone"></i><?= $fetch_order['number']; ?></p>
+											<p class="user"><i class="bi bi-envelope"></i><?= $fetch_order['email']; ?></p>
+											<p class="user"><i class="bi bi-pin-map-fill"></i><?= $fetch_order['address']; ?></p>
+											<p class="status">Statut : <span
+													data-status="<?= strtolower($fetch_order['status']); ?>"><?= $fetch_order['status']; ?></span>
+											</p>
+										</div>
+										<?php if ($fetch_order['status'] == 'annulee') { ?>
+											<a href="checkout.php?get_id=<?= $fetch_item['id']; ?>&type=<?= $fetch_order['item_type']; ?>"
+												class="btn">Commander à nouveau</a>
+										<?php } else { ?>
+											<form method="post">
+												<button type="submit" name="cancel" class="btn"
+													onclick="return confirm('Voulez-vous annuler cette commande ?')">Annuler la
+													commande</button>
+											</form>
+										<?php } ?>
+									</div>
 								</div>
-							</div>
-							<?php
+								<?php
+							}
+						} else {
+							echo '<p class="empty">Produit non trouvé</p>';
 						}
-					} else {
-						echo '<p class="empty">Produit non trouvé</p>';
 					}
+				} else {
+					echo '<p class="empty">Aucune commande trouvée</p>';
 				}
-			} else {
-				echo '<p class="empty">Aucune commande trouvée</p>';
-			}
-			?>
-			<!-- </div> -->
+				?>
+			</div>
 
 		</section>
 		<?php include 'components/footer.php'; ?>
