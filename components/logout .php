64691365e205551310b7a6ed12f2940d03e540e6
Hillel3009 <161851232+Hillel3009@users.noreply.github.com>
2024-05-03 18:50:27 +0200

    modif image user

--- components/logout .php
+++ components/logout .php
@@ -1,8 +1,13 @@
-<?php 
-	include 'components/connection.php';
+<?php
+include 'components/connection.php';
 
-	session_start();
-	session_unset();
-	session_destroy();
-	header('Location: home.php');
+if (headers_sent($file, $line)) {
+    echo "Les en-têtes ont été envoyés dans $file à la ligne $line";
+} else {
+    session_start();
+    session_unset();
+    session_destroy();
+    header('Location: /projet-php-e-commerce/home.php');
+    exit;
+}
 ?>
