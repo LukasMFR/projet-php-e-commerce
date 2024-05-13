d3c0ecc0111bd54379212727ee357fd0ac61ad32
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-13 19:23:12 +0200

    Add iOS stuff PWA

--- components/pwa-setup.php
+++ components/pwa-setup.php
@@ -1,5 +1,9 @@
 <?php
 echo '<link rel="manifest" href="/manifest.json">';
+echo '<meta name="apple-mobile-web-app-capable" content="yes">';
+echo '<meta name="apple-mobile-web-app-status-bar-style" content="black">';
+echo '<meta name="apple-mobile-web-app-title" content="Road Luxury">';
+echo '<link rel="apple-touch-icon" href="/img/icon-192x192.png">';
 echo '<script>';
 echo 'if ("serviceWorker" in navigator) {';
 echo '  navigator.serviceWorker.register("/service-worker.js").then(function(registration) {';
