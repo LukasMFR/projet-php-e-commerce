a5eed42aa02077fdb61ca3e2a3351b3313ff5a5c
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-13 18:58:32 +0200

    Add manifest.json & service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'road-luxury-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.php',
+    '/style.css',
+    '/script.js',
+    '/img/icon-192x192.png',
+    '/img/icon-512x512.png',
+    // Ajoutez ici toutes les autres ressources que vous souhaitez mettre en cache
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
