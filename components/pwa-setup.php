64255a6b2ff12ff11b1aabd10de46ae51efe5611
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-13 19:40:51 +0200

    Add more icons for PWA

--- components/pwa-setup.php
+++ components/pwa-setup.php
@@ -1,10 +1,15 @@
 <?php
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">';
 echo '<link rel="manifest" href="/manifest.json">';
 echo '<meta name="apple-mobile-web-app-capable" content="yes">';
 echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
 echo '<meta name="apple-mobile-web-app-title" content="Road Luxury">';
 echo '<link rel="apple-touch-icon" sizes="192x192" href="/img/icon-192x192.png">';
 echo '<link rel="apple-touch-icon" sizes="512x512" href="/img/icon-512x512.png">';
+echo '<link rel="apple-touch-icon" sizes="72x72" href="/icon/hd_hi.ico">';
+echo '<link rel="apple-touch-icon" sizes="96x96" href="/icon/hd_hi.ico">';
+echo '<link rel="apple-touch-icon" sizes="128x128" href="/icon/hd_hi.ico">';
+echo '<link rel="apple-touch-icon" sizes="256x256" href="/icon/hd_hi.ico">';
 echo '<script>';
 echo 'if ("serviceWorker" in navigator) {';
 echo '  navigator.serviceWorker.register("/service-worker.js").then(function(registration) {';
