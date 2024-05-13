f6d979c6b4723ebdbb63e709a97b56062df61bc9
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-13 19:05:23 +0200

    Move service worker call to dedicate file

---
+++ components/pwa-setup.php
@@ -0,0 +1,12 @@
+<?php
+echo '<link rel="manifest" href="/manifest.json">';
+echo '<script>';
+echo 'if ("serviceWorker" in navigator) {';
+echo '  navigator.serviceWorker.register("/service-worker.js").then(function(registration) {';
+echo '    console.log("Service Worker registered with scope:", registration.scope);';
+echo '  }).catch(function(error) {';
+echo '    console.log("Service Worker registration failed:", error);';
+echo '  });';
+echo '}';
+echo '</script>';
+?>
