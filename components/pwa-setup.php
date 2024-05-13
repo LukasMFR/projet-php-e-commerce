1391d68c4b90a19e17b9fdf910cf134223040b2c
Lukas MAUFFRÉ <41242166+LukasMFR@users.noreply.github.com>
2024-05-13 19:43:54 +0200

    Remove viewport line PWA

--- components/pwa-setup.php
+++ components/pwa-setup.php
@@ -1,5 +1,5 @@
 <?php
-echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">';
+// echo '<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">';
 echo '<link rel="manifest" href="/manifest.json">';
 echo '<meta name="apple-mobile-web-app-capable" content="yes">';
 echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
